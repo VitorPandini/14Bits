c5590b77b6070f6777b2831740903f07f66941fe
Priscila Hilario <hilario.prii@gmail.com>
2024-09-03 18:31:08 -0300

    não teve alterações no estilo

--- root/js/regiao.js
+++ root/js/regiao.js
@@ -1,18 +1,28 @@
 // Criando subtítulo e atribuindo conteúdo
 let subtitulo = document.createElement("h2");
-subtitulo.id = "indices-sudeste";
-subtitulo.innerText = "Tecnologia no Agronegócio";
+subtitulo.id = "região-sudeste";
+subtitulo.innerText = "Sobre a Região Sudeste: ";
 
 // Criando parágrafo e atribuindo conteúdo
 let paragrafo = document.createElement("p");
-paragrafo.innerText = `O Índice FIEC de Inovação dos Estados revela que São Paulo é o estado com o maior potencial de inovação do Brasil, tanto no ranking geral quanto nos indicadores específicos.
-O Rio de Janeiro ocupa a quarta colocação.
-A região Sudeste também concentra empresas de software agrícola.
-O Estudo do Mercado Brasileiro de Software para o Agronegócio, coordenado pela Embrapa Informática Agropecuária (Campinas, SP), aponta, em levantamento inicial, que existem no País 114 empresas desenvolvedoras de software para uso no setor agrícola. De acordo com o estudo, 60% delas estão concentradas na região Sudeste, especialmente em São Paulo e Minas Gerais.
-Em segundo lugar, está a região Sul, com 25% das empresas.
-`;
+paragrafo.innerText = `A região Sudeste do Brasil é composta pelos estados de São Paulo, Rio de Janeiro,
+Minas Gerais e Espírito Santo.
+É a mais industrializada e economicamente desenvolvida do país, abrigando importantes centros
+urbanos, culturais e financeiros. E é considerada uma das mais desenvolvidas tecnologicamente do Brasil, juntamente com a região Sul.
+Segundo o Ranking Connected Smart Cities, 58 das 100 cidades mais inteligentes e conectadas do país estão localizadas na região Sudeste.
+Além disso, é um polo tecnológico com diversas startups, centros de inovação e grandes empresas de
+tecnologia.
+A região Sudeste também é a mais rica do Brasil e possui a maior concentração populacional do país. A industrialização do país teve início na região Sudeste, e o estado de São Paulo é o principal centro industrial do Brasil. 
+Alguns dos ramos industriais mais importantes da região Sudeste são: Automobilística, Siderúrgica, Petroquímica, Naval, Petrolífera.
+oncentram-se 70% das despesas com desenvolvimento de tecnologias realizadas pelas empresas brasileiras.
+Somando-se as regiões Sudeste e Sul, esse percentual eleva-se para 90% (Braga e Matesco, 1986).
+
+Clique no link abaixo para descobrir mais sobre os demais estados da região Sudeste.     `;
 
 // Selecionando e adicionando elementos no DOM
 let section = document.getElementById("regiao-sudeste");
 section.appendChild(subtitulo);
 section.appendChild(paragrafo);
+
+
+
